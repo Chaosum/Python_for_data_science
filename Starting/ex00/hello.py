11b866f8689bc71d24de03a787ce5140bf829b3b
Matthieu Servagent <matthieu.servagent@Dymasco.com>
2025-06-01 00:02:27 +0200

    Starting/ex00/

---
+++ Starting/ex00/hello.py
@@ -0,0 +1,26 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+print("\n")
+
+#your code here
+
+
+
+ft_list[1] = "World!"
+ft_tuple = (ft_tuple[0], "France!")
+ft_set.remove("tutu!")
+ft_set.add("Charbonnière les bains!")
+ft_dict["Hello"] = "42 Lyon!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
